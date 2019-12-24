f1ce8f06ec4707df0b0173171a07ecceecf22e61
samuelrawrs <54682777+samuelrawrs@users.noreply.github.com>
2019-12-25 00:32:44 +0800

    Add files via upload

---
+++ Locomotion.ino
@@ -0,0 +1,221 @@
+#define IRlowspd 80
+#define IRhighspd 150
+#define IRbackspd 40
+
+#define IRlow 30
+#define IRhigh 110
+
+#define FRONTLIM 200
+#define LEFTLIM 25
+#define BACKLIM 25
+#define BACKGOAL 15
+#define RIGHTLIM 25
+
+/*
+boolean hasBall() {
+   return int(analogRead(LDR))<ldrthres;
+}
+*/
+
+void trackball() {
+  int tempspd=0, tempdir=0;
+  int rangeL=getRangeL(), rangeB=getRangeB(), rangeR=getRangeR();
+  //Serial.println(String(rangeL)+"\t"+String(rangeB)+"\t"+String(rangeR));
+  
+  if (bearing<=180 && bearing>compTolerance) northing = pow((bearing/20),2)+12;
+  else if (bearing>180 && bearing<360-compTolerance) northing = -(pow(((bearing-360)/20),2)+12);
+  else northing = 0;
+  /*
+  if (rangeL > LEFTLIM*1.2 && rangeR > RIGHTLIM*1.2) { // Within goal
+    BACKLIM = BACKGOAL;
+  }
+  */
+  if (IRstr < IRlow) { // Ball not detected
+    /*
+    tempspd = 0;
+    tempdir = 0;
+    */
+    
+    //tempspd = IRlowspd;
+    
+    if (rangeL > LEFTLIM && rangeR > RIGHTLIM) { //behind goal post
+      if(rangeB > BACKLIM) {
+        tempdir = 180;
+        tempspd = IRlowspd;
+      }
+      else {
+        tempdir = 0;
+        tempspd = 0;
+      }
+    }
+    else {
+      if(rangeB > BACKLIM*1.5) {
+        tempdir = 180;
+        tempspd = IRlowspd;
+        
+        if (rangeL < LEFTLIM) {
+          tempdir = 135;
+          tempspd = IRlowspd;
+        }
+        else if (rangeR < RIGHTLIM) {
+          tempdir = 225;
+          tempspd = IRlowspd;
+        }
+      }
+      else {
+        tempspd = 0;
+        tempdir = 0;
+      }
+    }
+  }
+  else if (IRstr >= IRlow && IRstr < IRhigh) { // Ball detected far away
+    
+    tempspd = IRlowspd;
+    
+    if (IRFB == 0) { // Ball in front
+      if (IRdir == 1) tempdir = 110;
+      else if (IRdir == 2) tempdir = 90;
+      else if (IRdir == 3) tempdir = 55;
+      else if (IRdir == 4) {tempdir = 0; tempspd = IRhighspd;}
+      else if (IRdir == 5) tempdir = 305;
+      else if (IRdir == 6) tempdir = 270;
+      else if (IRdir == 7) tempdir = 250;
+    }
+    else { // Ball behind
+      if (IRdir == 1) tempdir = 250;
+      else if (IRdir == 2) tempdir = 215;
+      else if (IRdir == 3) tempdir = 165;
+      else if (IRdir == 4) tempdir = 140;
+      else if (IRdir == 5) tempdir = 195;
+      else if (IRdir == 6) tempdir = 145;
+      else if (IRdir == 7) tempdir = 110;
+    }
+  }
+  else if (IRstr >= IRhigh) { // Ball close
+    tempspd = IRlowspd;
+    /*
+    if (hasBall()) {
+      if (IRdir == 4) { // Ball directly in front and in possession
+        tempdir = 0;
+        tempspd = IRhighspd;
+      }
+    }
+    */
+    //else { // Ball not in possession
+      if (IRFB == 0) { // Ball in front
+        if (IRdir == 1) tempdir = 105;
+        else if (IRdir == 2) tempdir = 95;
+        else if (IRdir == 3) tempdir = 40;
+        else if (IRdir == 4) {tempdir = 0; tempspd = IRhighspd;}
+        else if (IRdir == 5) tempdir = 320;
+        else if (IRdir == 6) tempdir = 265;
+        else if (IRdir == 7) tempdir = 255;
+      }
+      else { // Ball behind
+        tempspd = IRhighspd;
+        if (IRdir == 1) tempdir = 220;
+        else if (IRdir == 2) tempdir = 190;
+        else if (IRdir == 3) tempdir = 120;
+        else if (IRdir == 4) {
+          
+          if (rangeL < rangeR) tempdir = 260;
+          else tempdir = 100;
+          
+          //tempdir = 260;
+          //tempspd = IRhighspd;
+        }
+        else if (IRdir == 5) tempdir = 240;
+        else if (IRdir == 6) tempdir = 170;
+        else if (IRdir == 7) tempdir = 160;
+      }
+    //}
+  }
+  
+  
+  if (rangeL <= LEFTLIM) { // Too left
+    tempspd = IRlowspd;
+    
+    if (tempdir > 180) tempdir = 90;
+  }
+  else if (rangeB <= BACKLIM) {
+    tempspd = IRlowspd;
+    
+    if (tempdir > 90 && tempdir < 270) tempdir = 0;
+  }
+  else if (rangeR <= RIGHTLIM) {
+    tempspd = IRlowspd;
+    
+    if (tempdir < 180) tempdir = 270;
+  }
+  else if (rangeB >= FRONTLIM) { // Too front
+    tempspd = IRlowspd;
+    
+    if (tempdir > 270) tempdir = 260;
+    else if (tempdir < 90) tempdir = 100;
+    else if (tempdir == 0) tempdir = 180;
+  }
+  else if (rangeB <= BACKLIM) { // Too back
+    tempspd = IRlowspd;
+    
+    if (tempdir > 90 && tempdir < 270) tempdir = 0;
+  }
+  else if (rangeL > LEFTLIM && rangeR <= RIGHTLIM) { // Too right
+    tempspd = IRlowspd;
+  
+    if (rangeB > BACKLIM) { // Within BACKLIM
+      if (tempdir > 10 && tempdir < 90) tempdir = 0;
+      else if (tempdir >= 90 && tempdir < 180) tempdir = 180;
+    }
+    else { // (rangeB <= BACKLIM) Too back
+      if (tempdir > 10 && tempdir < 90) tempdir = 0;
+      else if (tempdir >= 90 && tempdir < 270) tempdir = 330;
+    }
+  }
+  else if (rangeR > RIGHTLIM && rangeL <= LEFTLIM) { // Too left
+    tempspd = IRlowspd;
+  
+    if (rangeB > BACKLIM) { // Within BACKLIM
+      if (tempdir > 270 && tempdir <= 350) tempdir = 0;
+      else if (tempdir > 190 && tempdir <= 270) tempdir = 180;
+    }
+    else { // (rangeB <= BACKLIM) // Too back
+      if (tempdir > 270 && tempdir <= 350) tempdir = 0;
+      else if (tempdir >= 90 && tempdir <= 270) tempdir = 30;
+    }
+  }
+  else if (rangeL <= LEFTLIM && rangeR <= RIGHTLIM) { // Sandwich
+    tempspd = IRlowspd;
+    if (tempdir < 90 || tempdir > 270) tempdir = 0;
+    else if (tempdir >= 90) tempdir = 180;
+  }
+  
+  /*  
+  // Ultrasonic Limitation
+    if (rangeR<=RIGHTLIM) {
+      if (tempdir>0 && tempdir<=90) tempdir=345;
+      else if (tempdir>90 && tempdir<180) tempdir=195;
+      //Serial.println(tempdir);
+    } else if (rangeR>=LEFTLIM) {
+      if (tempdir<360 && tempdir<270) tempdir=15;
+      else if (tempdir<270 && tempdir>180) tempdir=165;
+      //Serial.println(tempdir);
+    }
+    /*
+    if (rangeB=>FRONTLIM) {
+      tempdir=-1;
+    }// else if (rangeB<=BACKLIM) tempdir=-1;
+    
+    if (tempdir!=-1) setMove(tempspd,tempdir);
+    else setMove(0,0);
+    
+    } else {
+      if (rangeR<=RIGHTGOALLIM) setMove(IRhighspeed*1.2, 270);
+      else if (rangeR>=LEFTGOALLIM) setMove(IRhighspeed*1.2, 180);
+      else setMove(IRhighspeed*1.2,180);
+      //setMove(0,0);
+    }
+  }
+  */
+  setMove(tempspd,tempdir);
+  //Serial.print(tempspd);Serial.print("\t");Serial.println(tempdir);
+}
---
+++ Robocup_WX.ino
@@ -0,0 +1,167 @@
+#include <Wire.h>
+#include <NewPing.h>
+#include <math.h>
+
+// Compass Address
+#define CMPADDR 0x60
+#define compTolerance 6
+int compoffset=0, prevcompoffset=0, bearing=0;
+float northing=0;
+
+// Slave i2c port
+#define MicroADDR 121
+
+// IR Addresses
+#define IRADDR_F 0x02
+#define IRADDR_B 0x01
+int IRdir=0, IRstr=0, IRFB=0;
+
+// Ultrasound
+#define SONAR_NUM 3
+//int rangeUS[SONAR_NUM];
+
+// LDR Pin
+#define LDR A1
+int ldrthres=60;
+
+// Solenoid Pins
+#define SLN_1 6
+#define SLN_2 7
+void fire_SLN() {
+  analogWrite(SLN_1,255);
+  analogWrite(SLN_2,255);
+  delay(100);
+  
+  analogWrite(SLN_1,0);
+  analogWrite(SLN_2,0);
+}
+
+// Switches
+#define switch_spd A2
+#define switch_mode A3
+
+// Movement Pins
+//Robot 1
+#define PIN_FL 22
+#define PIN_FR 26
+#define PIN_BL 32
+#define PIN_BR 38
+
+#define PWM_FL 2
+#define PWM_FR 3
+#define PWM_BL 4
+#define PWM_BR 5
+
+#define FLBRfast 1.05
+int SmotorFL=0, SmotorFR=0, SmotorBL=0, SmotorBR=0;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  
+  pinMode(PIN_FL,OUTPUT);pinMode(PIN_FR,OUTPUT);pinMode(PIN_BL,OUTPUT);pinMode(PIN_BR,OUTPUT);
+  pinMode(PWM_FL,OUTPUT);pinMode(PWM_FR,OUTPUT);pinMode(PWM_BL,OUTPUT);pinMode(PWM_BR,OUTPUT);
+  analogWrite(PWM_FL,0);analogWrite(PWM_FR,0);analogWrite(PWM_BL,0);analogWrite(PWM_BR,0);
+  
+  /*
+  pinMode(SLN_1,OUTPUT);pinMode(SLN_2,OUTPUT);
+  fire_SLN();
+    
+  // Initialise LDR
+  ldrthres=analogRead(LDR)-ldrthres;
+  */
+  
+  //delay(100);
+  Serial.println("Initialising sensors");
+  Serial.println("1.Compass"); initComp();
+  Serial.println("2. Infraredlol"); initIR();
+  //Serial.println("3. Ultrasonic"); initUS();
+  Serial.println("Initialisation Complete!");
+}
+
+void setMove(int spd, float dir, int spin=0) {
+  /*
+    LOW - Anticlockwise (-ve)
+    HIGH - Clockwise    (+ve)
+  */
+  
+  if (dir>=360) dir-=360;
+  /*
+  if (bearing<=180 && bearing>compTolerance) northing = pow((bearing/20),2)+12;
+  else if (bearing>180 && bearing<360-compTolerance) northing = -(pow(((bearing-360)/20),2)+12);
+  else northing=0;
+  */
+  if (spd==0) { // not moving
+    SmotorFL = northing;
+    SmotorFR = northing;
+    SmotorBL = northing;
+    SmotorBR = northing;
+    //Serial.println(northing);
+  }
+  else { // moving
+    if (dir==0) {
+      SmotorFL = -spd;
+      SmotorFR = spd;
+      SmotorBL = -spd;
+      SmotorBR = spd;
+    }
+    else if (dir<=90) {
+      SmotorFL = -spd;
+      SmotorFR = spd*((35-dir)/55);
+      SmotorBL = -spd*((35-dir)/55);
+      SmotorBR = spd;
+    }
+    else if (dir<=180) {
+      SmotorFL = -spd*((125-dir)/55);
+      SmotorFR = -spd;
+      SmotorBL = spd;
+      SmotorBR = spd*((125-dir)/55);
+    }
+    else if (dir<=270) {
+      SmotorFL = spd;
+      SmotorFR = -spd*((215-dir)/55);
+      SmotorBL = spd*((215-dir)/55);
+      SmotorBR = -spd;
+    }
+    else { // (dir<360)
+      SmotorFL = spd*((305-dir)/55);
+      SmotorFR = spd;
+      SmotorBL = -spd;
+      SmotorBR = -spd*((305-dir)/55);
+    }
+  }
+  
+  // Calculate North Compensation
+  northing *= .1*spd/5;
+  SmotorFL += northing;
+  SmotorFR += northing;
+  SmotorBL += northing;
+  SmotorBR += northing;
+  
+  //Compensate FL and BR motors
+  SmotorFL *= FLBRfast;
+  SmotorBR *= FLBRfast;
+  
+  //Serial.println(String(SmotorBR)+"\t"+String(SmotorBL)+"\t"+String(SmotorFR)+"\t"+String(SmotorFL));
+  
+  // GO!
+  analogWrite(PWM_FL,abs(SmotorFL));digitalWrite(PIN_FL,SmotorFL>0?HIGH:LOW);
+  analogWrite(PWM_FR,abs(SmotorFR));digitalWrite(PIN_FR,SmotorFR>0?HIGH:LOW);
+  analogWrite(PWM_BL,abs(SmotorBL));digitalWrite(PIN_BL,SmotorBL>0?HIGH:LOW);
+  analogWrite(PWM_BR,abs(SmotorBR));digitalWrite(PIN_BR,SmotorBR>0?HIGH:LOW);
+}
+
+void loop() {
+  bearing = readComp();
+  
+  
+  readIR();
+
+  readUS();
+  
+  trackball();
+  
+  Serial.print(IRFB);Serial.print("\t");Serial.print(IRstr);Serial.print("\t");Serial.print(IRdir);Serial.print("\t");/*Serial.print();Serial.print("\t");*/Serial.println(bearing);
+
+  //Serial.print(getRangeL());Serial.print("\t");Serial.print(getRangeB());Serial.print("\t");Serial.println(getRangeR());
+}
